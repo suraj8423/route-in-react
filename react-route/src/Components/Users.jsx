16a14e3d8445a68f85c8cb2076b7b05df3b8f941
Suraj Tripathi <Suraj.Tripathi@leoforce.local>
2024-08-17 14:33:40 +0530

    search Params

--- react-route/src/Components/Users.jsx
+++ react-route/src/Components/Users.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active";
   return (
     <div>
       <Link to="1"> User 1</Link>
@@ -9,6 +11,19 @@ const Users = () => {
       <Link to="3"> User 3</Link>
       <Link to="admin">Admin</Link>
       <Outlet />
+
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button> onClick={() => setSearchParams({})}Reset Filter</button>
+      </div>
+
+      {showActiveUsers ? (
+        <h2> Showing Active Users</h2>
+      ) : (
+        <h2> Showing All Users</h2>
+      )}
     </div>
   );
 };
